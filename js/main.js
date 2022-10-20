ef93b955358e489a70964b21b6a5e9c4f3cb6654
bartvanmaarschalkerweerd <bart.vanmaarschalkerweerd@outlook.com>
2022-10-20 21:53:26 +0200

    Added hover on icons

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-let greetings = [];
-let answer = "";
-if(true) { answer = 'AY NAH'; } 
-else {
-  greetings = ['Hello!', 'Heyo!', 'yo', 'hi', 'hello', ''];
-  answer = greetings[Math.floor(Math.random() * greetings.length)]
-}
-console.log(answer);
+console.log('Deze website is nog in ontwikkeling 😁')
